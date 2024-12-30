67daf4348dd9d7c8e385b7323633f024e94be7d3
Persephone Wilder <shaneawilder@gmail.com>
2024-12-30 13:44:42 -0600

    Changed placement of bases to hopefully make it clear to use them first

--- DCGUI.py
+++ DCGUI.py
@@ -362,52 +362,52 @@ class CalculatorGUI:
         self.buttonframe.columnconfigure(4, weight=1)
         
         btn1 = tk.Button(self.buttonframe, text="1", font=('Arial', 16), command=self.add1)
-        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        btn1.grid(row=1, column=0, sticky=tk.W+tk.E)
         
         btn2 = tk.Button(self.buttonframe, text="2", font=('Arial', 16), command=self.add2)
-        btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+        btn2.grid(row=1, column=1, sticky=tk.W+tk.E)
         
         btn3 = tk.Button(self.buttonframe, text="3", font=('Arial', 16), command=self.add3)
-        btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
+        btn3.grid(row=1, column=2, sticky=tk.W+tk.E)
         
         btn4 = tk.Button(self.buttonframe, text="4", font=('Arial', 16), command=self.add4)
-        btn4.grid(row=0, column=3, sticky=tk.W+tk.E)
+        btn4.grid(row=1, column=3, sticky=tk.W+tk.E)
         
         btn5 = tk.Button(self.buttonframe, text="5", font=('Arial', 16), command=self.add5)
-        btn5.grid(row=1, column=0, sticky=tk.W+tk.E)
+        btn5.grid(row=2, column=0, sticky=tk.W+tk.E)
         
         btn6 = tk.Button(self.buttonframe, text="6", font=('Arial', 16), command=self.add6)
-        btn6.grid(row=1, column=1, sticky=tk.W+tk.E)
+        btn6.grid(row=2, column=1, sticky=tk.W+tk.E)
         
         btn7 = tk.Button(self.buttonframe, text="7", font=('Arial', 16), command=self.add7)
-        btn7.grid(row=1, column=2, sticky=tk.W+tk.E)
+        btn7.grid(row=2, column=2, sticky=tk.W+tk.E)
         
         btn8 = tk.Button(self.buttonframe, text="8", font=('Arial', 16), command=self.add8)
-        btn8.grid(row=1, column=3, sticky=tk.W+tk.E)
+        btn8.grid(row=2, column=3, sticky=tk.W+tk.E)
         
         btn9 = tk.Button(self.buttonframe, text="9", font=('Arial', 16), command=self.add9)
-        btn9.grid(row=2, column=0, sticky=tk.W+tk.E)
+        btn9.grid(row=3, column=0, sticky=tk.W+tk.E)
         
         btnA = tk.Button(self.buttonframe, text="A", font=('Arial', 16), command=self.addA)
-        btnA.grid(row=2, column=1, sticky=tk.W+tk.E)
+        btnA.grid(row=3, column=1, sticky=tk.W+tk.E)
         
         btnB = tk.Button(self.buttonframe, text="B", font=('Arial', 16), command=self.addB)
-        btnB.grid(row=2, column=2, sticky=tk.W+tk.E)
+        btnB.grid(row=3, column=2, sticky=tk.W+tk.E)
         
         btnC = tk.Button(self.buttonframe, text="C", font=('Arial', 16), command=self.addC)
-        btnC.grid(row=2, column=3, sticky=tk.W+tk.E)
+        btnC.grid(row=3, column=3, sticky=tk.W+tk.E)
         
         btnD = tk.Button(self.buttonframe, text="D", font=('Arial', 16), command=self.addD)
-        btnD.grid(row=3, column=0, sticky=tk.W+tk.E)
+        btnD.grid(row=4, column=0, sticky=tk.W+tk.E)
         
         btnE = tk.Button(self.buttonframe, text="E", font=('Arial', 16), command=self.addE)
-        btnE.grid(row=3, column=1, sticky=tk.W+tk.E)
+        btnE.grid(row=4, column=1, sticky=tk.W+tk.E)
         
         btnF = tk.Button(self.buttonframe, text="F", font=('Arial', 16), command=self.addF)
-        btnF.grid(row=3, column=2, sticky=tk.W+tk.E)
+        btnF.grid(row=4, column=2, sticky=tk.W+tk.E)
         
         btn0 = tk.Button(self.buttonframe, text="0", font=('Arial', 16), command=self.add0)
-        btn0.grid(row=3, column=3, sticky=tk.W+tk.E)
+        btn0.grid(row=4, column=3, sticky=tk.W+tk.E)
         
         btnAdd = tk.Button(self.buttonframe, text="+", font=('Arial', 16), command=self.setAdd)
         btnAdd.grid(row=0, column=4, sticky=tk.W+tk.E)
@@ -422,16 +422,16 @@ class CalculatorGUI:
         btnDiv.grid(row=3, column=4, sticky=tk.W+tk.E)
         
         btnDeci = tk.Button(self.buttonframe, text="Decimal", font=('Arial', 16), command=self.setDecimal)
-        btnDeci.grid(row=4, column=0, sticky=tk.W+tk.E)
+        btnDeci.grid(row=0, column=0, sticky=tk.W+tk.E)
         
         btnBinary = tk.Button(self.buttonframe, text="Binary", font=('Arial', 16), command=self.setBinary)
-        btnBinary.grid(row=4, column=1, sticky=tk.W+tk.E)
+        btnBinary.grid(row=0, column=1, sticky=tk.W+tk.E)
         
         btnOct = tk.Button(self.buttonframe, text="Octal", font=('Arial', 16), command=self.setOctal)
-        btnOct.grid(row=4, column=2, sticky=tk.W+tk.E)
+        btnOct.grid(row=0, column=2, sticky=tk.W+tk.E)
         
         btnHex = tk.Button(self.buttonframe, text="Hexadecimal", font=('Arial', 16), command=self.setHexadecimal)
-        btnHex.grid(row=4, column=3, sticky=tk.W+tk.E)
+        btnHex.grid(row=0, column=3, sticky=tk.W+tk.E)
         
         btnClear = tk.Button(self.buttonframe, text="Clear", font=("Arial", 16), command=self.clear)
         btnClear.grid(row=5, column=0, sticky=tk.W+tk.E)
