5322ba5680ec6baf52876935dd61b3a59fe361a1
Persephone Wilder <shaneawilder@gmail.com>
2024-12-29 22:46:40 -0600

    Added everything to a class structure

--- DCGUI.py
+++ DCGUI.py
@@ -1,98 +1,77 @@
 import tkinter as tk
 from DecimalConverter import DecimalConverter as DC
 
-root = tk.Tk()
+class CalculatorGUI:
+    def __init__(self):
+        
+        self.root = tk.Tk()
+        self.root.geometry("450x600") 
+        self.root.title("Decimal Converter")
+        self.textbox = tk.Text(self.root, font=("Arial", 20), height=1, width=25)
+        self.textbox.pack()
+        self.buttonframe = tk.Frame(self.root)
+        self.addButtons()
+        
+        self.root.mainloop()
+        
+    def addButtons(self):
+        self.buttonframe.columnconfigure(0, weight=1)
+        self.buttonframe.columnconfigure(1, weight=1)
+        self.buttonframe.columnconfigure(2, weight=1)
+        self.buttonframe.columnconfigure(3, weight=1)
+        self.buttonframe.columnconfigure(4, weight=1)
+        
+        btn1 = tk.Button(self.buttonframe, text="1", font=('Arial', 16))
+        btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        btn2 = tk.Button(self.buttonframe, text="2", font=('Arial', 16))
+        btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+        btn3 = tk.Button(self.buttonframe, text="3", font=('Arial', 16))
+        btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
+        btn4 = tk.Button(self.buttonframe, text="4", font=('Arial', 16))
+        btn4.grid(row=0, column=3, sticky=tk.W+tk.E)
+        btn5 = tk.Button(self.buttonframe, text="5", font=('Arial', 16))
+        btn5.grid(row=1, column=0, sticky=tk.W+tk.E)
+        btn6 = tk.Button(self.buttonframe, text="6", font=('Arial', 16))
+        btn6.grid(row=1, column=1, sticky=tk.W+tk.E)
+        btn7 = tk.Button(self.buttonframe, text="7", font=('Arial', 16))
+        btn7.grid(row=1, column=2, sticky=tk.W+tk.E)
+        btn8 = tk.Button(self.buttonframe, text="8", font=('Arial', 16))
+        btn8.grid(row=1, column=3, sticky=tk.W+tk.E)
+        btn9 = tk.Button(self.buttonframe, text="9", font=('Arial', 16))
+        btn9.grid(row=2, column=0, sticky=tk.W+tk.E)
+        btn10 = tk.Button(self.buttonframe, text="A", font=('Arial', 16))
+        btn10.grid(row=2, column=1, sticky=tk.W+tk.E)
+        btn11 = tk.Button(self.buttonframe, text="B", font=('Arial', 16))
+        btn11.grid(row=2, column=2, sticky=tk.W+tk.E)
+        btn12 = tk.Button(self.buttonframe, text="C", font=('Arial', 16))
+        btn12.grid(row=2, column=3, sticky=tk.W+tk.E)
+        btn13 = tk.Button(self.buttonframe, text="D", font=('Arial', 16))
+        btn13.grid(row=3, column=0, sticky=tk.W+tk.E)
+        btn14 = tk.Button(self.buttonframe, text="E", font=('Arial', 16))
+        btn14.grid(row=3, column=1, sticky=tk.W+tk.E)
+        btn15 = tk.Button(self.buttonframe, text="F", font=('Arial', 16))
+        btn15.grid(row=3, column=2, sticky=tk.W+tk.E)
+        btn16 = tk.Button(self.buttonframe, text="0", font=('Arial', 16))
+        btn16.grid(row=3, column=3, sticky=tk.W+tk.E)
+        btn17 = tk.Button(self.buttonframe, text="+", font=('Arial', 16))
+        btn17.grid(row=0, column=4, sticky=tk.W+tk.E)
+        btn18 = tk.Button(self.buttonframe, text="-", font=('Arial', 16))
+        btn18.grid(row=1, column=4, sticky=tk.W+tk.E)
+        btn19 = tk.Button(self.buttonframe, text="*", font=('Arial', 16))
+        btn19.grid(row=2, column=4, sticky=tk.W+tk.E)
+        btn20 = tk.Button(self.buttonframe, text="/", font=('Arial', 16))
+        btn20.grid(row=3, column=4, sticky=tk.W+tk.E)
+        btn21 = tk.Button(self.buttonframe, text="Decimal", font=('Arial', 16))
+        btn21.grid(row=4, column=0, sticky=tk.W+tk.E)
+        btn22 = tk.Button(self.buttonframe, text="Binary", font=('Arial', 16))
+        btn22.grid(row=4, column=1, sticky=tk.W+tk.E)
+        btn23 = tk.Button(self.buttonframe, text="Octal", font=('Arial', 16))
+        btn23.grid(row=4, column=2, sticky=tk.W+tk.E)
+        btn24 = tk.Button(self.buttonframe, text="Hexadecimal", font=('Arial', 16))
+        btn24.grid(row=4, column=3, sticky=tk.W+tk.E)
+        btn25 = tk.Button(self.buttonframe, text="Clear", font=("Arial", 16))
+        btn25.grid(row=4, column=4, sticky=tk.W+tk.E)
+        self.buttonframe.pack(fill='both')
 
-root.geometry("450x600") 
-root.title("Decimal Converter")
-
-textbox = tk.Text(root, font=("Arial", 14), height=1, width=25)
-textbox.pack()
-
-input = tk.Entry(root)
-input.pack()
-
-buttonframe = tk.Frame(root)
-buttonframe.columnconfigure(0, weight=1)
-buttonframe.columnconfigure(1, weight=1)
-buttonframe.columnconfigure(2, weight=1)
-buttonframe.columnconfigure(3, weight=1)
-buttonframe.columnconfigure(4, weight=1)
-
-btn1 = tk.Button(buttonframe, text="1", font=('Arial', 16))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
-
-btn2 = tk.Button(buttonframe, text="2", font=('Arial', 16))
-btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
-
-btn3 = tk.Button(buttonframe, text="3", font=('Arial', 16))
-btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
-
-btn4 = tk.Button(buttonframe, text="4", font=('Arial', 16))
-btn4.grid(row=0, column=3, sticky=tk.W+tk.E)
-
-btn5 = tk.Button(buttonframe, text="5", font=('Arial', 16))
-btn5.grid(row=1, column=0, sticky=tk.W+tk.E)
-
-btn6 = tk.Button(buttonframe, text="6", font=('Arial', 16))
-btn6.grid(row=1, column=1, sticky=tk.W+tk.E)
-
-btn7 = tk.Button(buttonframe, text="7", font=('Arial', 16))
-btn7.grid(row=1, column=2, sticky=tk.W+tk.E)
-
-btn8 = tk.Button(buttonframe, text="8", font=('Arial', 16))
-btn8.grid(row=1, column=3, sticky=tk.W+tk.E)
-
-btn9 = tk.Button(buttonframe, text="9", font=('Arial', 16))
-btn9.grid(row=2, column=0, sticky=tk.W+tk.E)
-
-btn10 = tk.Button(buttonframe, text="A", font=('Arial', 16))
-btn10.grid(row=2, column=1, sticky=tk.W+tk.E)
-
-btn11 = tk.Button(buttonframe, text="B", font=('Arial', 16))
-btn11.grid(row=2, column=2, sticky=tk.W+tk.E)
-
-btn12 = tk.Button(buttonframe, text="C", font=('Arial', 16))
-btn12.grid(row=2, column=3, sticky=tk.W+tk.E)
-
-btn13 = tk.Button(buttonframe, text="D", font=('Arial', 16))
-btn13.grid(row=3, column=0, sticky=tk.W+tk.E)
-
-btn14 = tk.Button(buttonframe, text="E", font=('Arial', 16))
-btn14.grid(row=3, column=1, sticky=tk.W+tk.E)
-
-btn15 = tk.Button(buttonframe, text="F", font=('Arial', 16))
-btn15.grid(row=3, column=2, sticky=tk.W+tk.E)
-
-btn16 = tk.Button(buttonframe, text="0", font=('Arial', 16))
-btn16.grid(row=3, column=3, sticky=tk.W+tk.E)
-
-btn17 = tk.Button(buttonframe, text="+", font=('Arial', 16))
-btn17.grid(row=0, column=4, sticky=tk.W+tk.E)
-
-btn18 = tk.Button(buttonframe, text="-", font=('Arial', 16))
-btn18.grid(row=1, column=4, sticky=tk.W+tk.E)
-
-btn19 = tk.Button(buttonframe, text="*", font=('Arial', 16))
-btn19.grid(row=2, column=4, sticky=tk.W+tk.E)
-
-btn20 = tk.Button(buttonframe, text="/", font=('Arial', 16))
-btn20.grid(row=3, column=4, sticky=tk.W+tk.E)
-
-btn21 = tk.Button(buttonframe, text="Decimal", font=('Arial', 16))
-btn21.grid(row=4, column=0, sticky=tk.W+tk.E)
-
-btn22 = tk.Button(buttonframe, text="Binary", font=('Arial', 16))
-btn22.grid(row=4, column=1, sticky=tk.W+tk.E)
-
-btn23 = tk.Button(buttonframe, text="Octal", font=('Arial', 16))
-btn23.grid(row=4, column=2, sticky=tk.W+tk.E)
-
-btn24 = tk.Button(buttonframe, text="Hexadecimal", font=('Arial', 16))
-btn24.grid(row=4, column=3, sticky=tk.W+tk.E)
-
-buttonframe.pack(fill='both')
-
-tk.Checkbutton
-
-root.mainloop()
+if __name__ == "__main__":
+    CalculatorGUI()
